278cc5f755e415b9c402ba3614cd99d6e8d12426
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-08-14 20:34:01 +0300

    Create DFS.py

---
+++ Python/Algorithms/DFS.py
@@ -0,0 +1,16 @@
+def DFS(start_node, key):
+    next_nodes = []
+    seen_nodes = set()
+
+    next_nodes.append(start_node)
+    seen_nodes.add(start_node)
+
+    while next_nodes:
+        node = next_nodes.pop()
+        if node == key:
+            return node
+        for n in node:
+            if n not in seen_nodes:
+                next_nodes.append(n)
+                seen_nodes.add(n)
+    return None
