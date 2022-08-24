c18d45f65d7bdded33d27f4a3f89328c2e54db6e
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-08-24 18:25:50 +0300

    Add files via upload

---
+++ C++/SeaBattle/SeaBattle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "SeaBattle start!" << std::endl;
+    std::cout << "Type something";
+    std::string text;
+    std::cin >> text;
+    std::cout << "You have typed: " << text;
+    return 0;
+} 
