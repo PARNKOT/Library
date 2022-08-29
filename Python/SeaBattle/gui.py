b1c1a7101a32ad2f8c10bbd7dc65afefced8de14
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-08-29 16:09:34 +0300

    Update gui.py

--- Python/SeaBattle/gui.py
+++ Python/SeaBattle/gui.py
@@ -16,26 +16,55 @@ class NButton(ttk.Frame):
         self.button.pack(expand=1)
 
 
+class GamePoleGui(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        ttk.Style().configure('GamePole.TFrame', background='blue')
+        ttk.Style().configure('Cell.TButton', background='blue', foreground='red', relief='flat')
+        self.configure(style='GamePole.TFrame', padding=5)
+
+        self.cells = [[ttk.Button(self) for _ in range(10)] for _ in range(10)]
+        self.make_pole()
+
+    def make_pole(self):
+        for row in self.cells:
+            for cell in row:
+                cell.configure(width=5, style='Cell.TButton')
+
+        for row in range(10):
+            for column in range(10):
+                self.cells[row][column].grid(row=row, column=column, ipady=4)
+
+
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # MainWindow
         self.geometry(f'{GuiOptions.MAINWINDOW_WIDTH}x{GuiOptions.MAINWINDOW_HEIGHT}')
+        self.title('SeaBattle')
 
         # Frames
         self.main_frame = ttk.Frame(self, padding=10)
         self.menu_frame = ttk.Frame(self, padding=10)
         self.menu_frame.propagate(False)
+        self.main_frame.propagate(False)
+
+        # Separator
+        ttk.Style().configure('Sep.TSeparator', background='black')
+        self.separator = ttk.Separator(self, orient=tk.VERTICAL, style='Sep.TSeparator')
 
         # Buttons
-        #self.start_button = NButton(self.menu_frame)
         self.start_button = ttk.Button(self.menu_frame)
 
         # Styles
         ttk.Style().configure('Menu.TFrame', background='green')
         ttk.Style().configure('Main.TFrame', background='#f5f5f5')
         ttk.Style().configure('Menu.TButton')
+
+        # GamePoles
+        self.gamepole_player1 = GamePoleGui(self.main_frame)
+        self.gamepole_player2 = GamePoleGui(self.main_frame)
 
     def init_frames(self):
         # Configuring
@@ -46,16 +75,23 @@ class MainWindow(tk.Tk):
 
         # Packing
         self.menu_frame.pack(side=tk.LEFT, fill='both')
+        self.separator.pack(side=tk.LEFT, fill='both')
         self.main_frame.pack(side=tk.LEFT, fill='both')
 
     def init_buttons(self):
         self.start_button.configure(text='START')
-        self.start_button.pack(fill='x')
+        self.start_button.pack(fill='x', ipady=50, pady=10)
 
+    def init_gamepoles(self):
+        #self.gamepole_player1.configure(style='GamePole.TFrame', height=335, width=400)
+        #self.gamepole_player2.configure(style='GamePole.TFrame', height=335, width=400)
+        self.gamepole_player1.pack(side=tk.TOP)
+        self.gamepole_player2.pack(side=tk.BOTTOM)
 
     def run(self):
         self.init_frames()
         self.init_buttons()
+        self.init_gamepoles()
         self.mainloop()
 
 
