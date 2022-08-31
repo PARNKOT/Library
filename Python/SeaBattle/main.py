bfaa5138054fe71aefc7bf8bf365caf184fa2c2f
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-08-31 18:33:59 +0300

    Update main.py

--- Python/SeaBattle/main.py
+++ Python/SeaBattle/main.py
@@ -1,5 +1,6 @@
 import gui
 import backend as sb
+from functools import partial
 
 
 class SeaBattle:
@@ -11,6 +12,14 @@ class SeaBattle:
         # configure
         self.gui_app.start_button.configure(command=self.start)
         self.gui_app.random_button.configure(command=self.generate_ships)
+        self.define_cells_action(self.gui_app.gamepolegui_player2)
+        self.define_cells_action(self.gui_app.gamepolegui_player1)
+
+    def define_cells_action(self, gamepolegui: gui.GamePoleGui):
+        for row_index in range(10):
+            for column_index in range(10):
+                gamepolegui.cells[row_index][column_index].configure(
+                    command=partial(self.hit, sb.Point(column_index, row_index)))
 
     def generate_ships(self):
         self.gui_app.gamepolegui_player2.clear()
@@ -29,6 +38,11 @@ class SeaBattle:
 
     def open_app(self):
         self.gui_app.run()
+
+    def hit(self, point: sb.Point):
+        if self.gp2.is_ship_on_point(point):
+            self.gui_app.gamepolegui_player2.cells[point.y][point.x].configure(style=gui.STYLES['hitted_cell'])
+
 
 
 if __name__ == "__main__":
