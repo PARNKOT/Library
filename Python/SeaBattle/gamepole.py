56b43cba1828798d70a9341310aaaa1441fc20ec
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-09-12 16:22:12 +0300

    Update gamepole.py

--- Python/SeaBattle/gamepole.py
+++ Python/SeaBattle/gamepole.py
@@ -1,14 +1,15 @@
-import random
 from typing import List, Set
-from utils import Queue, Point, CoordConverter
+
+import GameOptions as options
 from ship import (
         Ship,
         OneDeckShip,
         TwoDeckShip,
         ThreeDeckShip,
         FourDeckShip,
-        GameOptions
     )
+from GamePoleOwner import ShipsMover, ShipConstellator
+from utils import Point
 
 
 class GamePole:
@@ -23,37 +24,18 @@ class GamePole:
     def init(self):
         self._ships.clear()
         ships = [
-            [OneDeckShip() for _ in range(GameOptions.ONE_DECKS)],
-            [TwoDeckShip() for _ in range(GameOptions.TWO_DECKS)],
-            [ThreeDeckShip() for _ in range(GameOptions.THREE_DECKS)],
-            [FourDeckShip() for _ in range(GameOptions.FOUR_DECKS)],
+            [OneDeckShip() for _ in range(options.ONE_DECKS)],
+            [TwoDeckShip() for _ in range(options.TWO_DECKS)],
+            [ThreeDeckShip() for _ in range(options.THREE_DECKS)],
+            [FourDeckShip() for _ in range(options.FOUR_DECKS)],
         ]
         for ship_type in ships:
             self._ships.extend(ship_type)
 
         ShipConstellator(self).place_ships()
-        self.update_pole()
 
     def move_ships(self):
         self.ships_mover.move_all_ships()
-        self.update_pole()
-
-    def update_pole(self):
-        self.__pole = [[0 for _ in range(self._size)] for _ in range(self._size)]
-        for ship in self._ships:
-            ship_cells_coords = ship.get_all_cells_of_ship()
-            for point in ship_cells_coords:
-                self.__pole[point.y][point.x] = 1
-
-    def show(self):
-        print('-'*15, ' Pole ', '-'*15, sep='')
-        for row in self.__pole:
-            for el in row:
-                if el == 0:
-                    print('- ', end='')
-                else:
-                    print('* ', end='')
-            print()
 
     def get_ships(self):
         return self._ships
@@ -84,110 +66,13 @@ class GamePole:
             restricted_cells.update(neighbours)
         return restricted_cells
 
-    def find_hitted_point(self, ship, point: Point) -> Point:
-        for cell in ship.get_all_cells_of_ship():
-            if point == cell:
-                return cell
-
-    def is_ship_on_point(self, point: Point):
-        for ship in self._ships:
-            if point in ship.get_all_cells_of_ship():
-                return True
-        return False
-
     def hit(self, point: Point):
         for ship in self._ships:
-            hitted_point = self.find_hitted_point(ship, point)
-            if hitted_point:
-                hitted_point.isHitted = True
-                ship._is_move = False
+            if point in ship.get_all_cells_of_ship():
+                ship.destroy_deck_on(point)
+                ship.is_move = False
                 break
         print(f'Hit on {point}')
-
-
-class GamePoleOwner:
-    def __init__(self, gamepole: GamePole):
-        self.gamepole = gamepole
-
-    @property
-    def ships(self):
-        return self.gamepole._ships
-
-    @property
-    def size(self):
-        return self.gamepole._size
-
-
-class ShipsMover(GamePoleOwner):
-    def __init__(self, gamepole: GamePole):
-        super().__init__(gamepole)
-
-    def move_all_ships(self, step=1):
-        self(step)
-
-    def move_forward(self, ship, step):
-        ship.move(step)
-        if self.gamepole.is_collision(ship) or ship.is_out_pole(self.size):
-            ship.move(-step)
-            return False
-        return True
-
-    def move_backward(self, ship, step):
-        return self.move_forward(ship, -step)
-
-    def __call__(self, step=1):
-        move_queue = Queue(self.ships)
-
-        count = 0
-        while move_queue:
-            ship_to_move = move_queue.pop()
-            if self.move_forward(ship_to_move, step):
-                count = 0
-            else:
-                if self.move_backward(ship_to_move, step):
-                    count = 0
-                else:
-                    move_queue.add(ship_to_move)
-                    count += 1
-
-            if count == len(move_queue):
-                break
-
-
-class ShipConstellator(GamePoleOwner):
-    def __init__(self, gamepole: GamePole):
-        super().__init__(gamepole)
-        self.allowed_cells = list(range(self.size * self.size))
-
-    def place_ship(self, ship):
-        ship.orientation = random.choice([GameOptions.HORIZONTAL, GameOptions.VERTICAL])
-        local_allowed_cells = self.allowed_cells.copy()
-        while local_allowed_cells:
-            position_absolute = random.choice(local_allowed_cells)
-            start_point = CoordConverter.absolute_to_matrix(position_absolute, self.size)
-            ship.start_point = start_point
-            if self.gamepole.is_collision(ship) or ship.is_out_pole(self.size):
-                ship.start_point = Point(None, None)
-                local_allowed_cells.remove(position_absolute)
-            else:
-                break
-
-        if ship.start_point.isEmpty():
-            raise ValueError('Не удалось разместить корабль. Попробуйте снова')
-
-        restricted_area = self.gamepole.get_restricted_ship_area(ship)
-        for point in restricted_area:
-            abs_cell = CoordConverter.matrix_to_absolute(point, self.size)
-            if abs_cell in self.allowed_cells:
-                self.allowed_cells.remove(abs_cell)
-
-    def place_ships(self):
-        for ship in self.ships:
-            try:
-                self.place_ship(ship)
-            except ValueError:
-                ship.reverse_orientation()
-                self.place_ship(ship)
 
 
 if __name__ == "__main__":
